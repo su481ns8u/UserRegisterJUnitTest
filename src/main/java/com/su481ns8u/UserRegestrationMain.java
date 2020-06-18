844e512ab6fffd71b96ab69586a8c64bf432bb9a
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-19 01:25:16 +0530

    ADDED validation for last name REFACTORED test for first name

--- src/main/java/com/su481ns8u/UserRegestrationMain.java
+++ src/main/java/com/su481ns8u/UserRegestrationMain.java
@@ -5,4 +5,8 @@ public class UserRegestrationMain {
     public boolean firstNameValidation (String fname) {
         return fname.matches("^[A-Z]{1}[a-zA-Z]{2,}");
     }
+
+    public boolean lastNameValidation (String lname) {
+        return lname.matches("^[A-Z]{1}[a-zA-Z]{2,}");
+    }
 }
--- src/test/java/com/su481ns8u/UserRegestrationJUnitTest.java
+++ src/test/java/com/su481ns8u/UserRegestrationJUnitTest.java
@@ -2,26 +2,55 @@ package com.su481ns8u;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class UserRegestrationJUnitTest {
     UserRegestrationMain userRegestrationMain = new UserRegestrationMain();
-    boolean result;
 
-    @Test
-    public void fistNameValidation_returnsTrue_ifFirstNameProper() {
-        result = userRegestrationMain.firstNameValidation("Sudhanshu");
-        Assert.assertTrue(result);
+    private String keys;
+    private boolean values;
+
+    public UserRegestrationJUnitTest(String keys, boolean values){
+        this.keys = keys;
+        this.values = values;
+    }
+
+    /* ----- First Name Validation ----- */
+    @Parameterized.Parameters
+    public static Collection<Object[]> testForFName(){
+        Object[][] testingFirstNames = new Object[][]{
+                {"Sudhanshu", true},
+                {"sudhanshu", false},
+                {"Su",false}
+        };
+        return Arrays.asList(testingFirstNames);
     }
 
     @Test
-    public void firstNameValidation_returnsFalse_ifNumberOfCharsDosentMatch() {
-        result = userRegestrationMain.firstNameValidation("Su");
-        Assert.assertFalse(result);
+    public void validateFirstName() {
+        boolean result = userRegestrationMain.firstNameValidation(keys);
+        Assert.assertEquals(result,values);
+    }
+
+    /* ---- Last Name Validation ----- */
+    @Parameterized.Parameters
+    public static Collection<Object[]> testForLName(){
+        Object[][] testingLastNames = new Object[][]{
+                {"Ghinmine", true},
+                {"ghinmine", false},
+                {"gh",false}
+        };
+        return Arrays.asList(testingLastNames);
     }
 
     @Test
-    public void firstNameValidation_returnsFalse_ifFirstCharNotUpper() {
-        result = userRegestrationMain.firstNameValidation("sudhanshu");
-        Assert.assertFalse(result);
+    public void validateLastName() {
+        boolean result = userRegestrationMain.lastNameValidation(keys);
+        Assert.assertEquals(result,values);
     }
 }
