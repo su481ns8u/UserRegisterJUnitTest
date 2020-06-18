2e121ecc9696c9c559019d9ddd17768bba1dedd6
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-19 00:06:33 +0530

    ADDED validation for first name

--- src/main/java/com/su481ns8u/UserRegestrationMain.java
+++ src/main/java/com/su481ns8u/UserRegestrationMain.java
@@ -2,4 +2,7 @@ package com.su481ns8u;
 
 public class UserRegestrationMain {
 
+    public boolean firstNameValidation (String fname) {
+        return fname.matches("^[A-Z]{1}[a-zA-Z]{2,}");
+    }
 }
--- src/test/java/com/su481ns8u/UserRegestrationJUnitTest.java
+++ src/test/java/com/su481ns8u/UserRegestrationJUnitTest.java
@@ -1,15 +1,27 @@
 package com.su481ns8u;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegestrationJUnitTest {
+    UserRegestrationMain userRegestrationMain = new UserRegestrationMain();
+    boolean result;
+
     @Test
-    public void name() {
-        UserRegestrationMain userRegestrationMain = new UserRegestrationMain();
+    public void fistNameValidation_returnsTrue_ifFirstNameProper() {
+        result = userRegestrationMain.firstNameValidation("Sudhanshu");
+        Assert.assertTrue(result);
     }
 
     @Test
-    public void NameTest() {
+    public void firstNameValidation_returnsFalse_ifNumberOfCharsDosentMatch() {
+        result = userRegestrationMain.firstNameValidation("Su");
+        Assert.assertFalse(result);
+    }
 
+    @Test
+    public void firstNameValidation_returnsFalse_ifFirstCharNotUpper() {
+        result = userRegestrationMain.firstNameValidation("sudhanshu");
+        Assert.assertFalse(result);
     }
 }
